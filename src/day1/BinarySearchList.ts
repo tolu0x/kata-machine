bdeae8d9bb7f8ff0f87189633094ce62a0da2d83
Toluwalase Adams <95189430+tolu0x@users.noreply.github.com>
2024-01-04 22:09:37 +0100

    Binary Search List

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let hi = haystack.length;
+  let lo = 0
 
+  do {
+    const mid = Math.floor(lo + (hi - lo) / 2);
+
+    if (needle === haystack[mid]) {
+      return true
+    } else if (needle > haystack[mid]) {
+      lo = mid + 1;
+    } else if (needle < haystack[mid]) {
+      hi = mid;
+    }
+  } while (lo < hi);
+
+  return false;
 }
