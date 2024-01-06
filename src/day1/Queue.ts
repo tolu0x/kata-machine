b1d280d85fac9a0fa8bc99ee450dea3b3166da3b
Toluwalase Adams <95189430+tolu0x@users.noreply.github.com>
2024-01-06 22:17:04 +0100

    Queue DS

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.length++
+    this.tail.next = node;
+    this.tail = node;
+  }
+  deque(): T | undefined {
+    this.length--;
+    if (!this.tail) {
+      return undefined;
+    }
 
-}
-    deque(): T | undefined {
+    const headValue = this.head;
+    this.head = this.head?.next;
 
-}
-    peek(): T | undefined {
-
-}
+    return headValue?.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
