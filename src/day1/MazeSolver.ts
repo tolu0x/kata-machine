13d123b5924b61f9e66f5d7453fe4d2df80691de
Toluwalase Adams <95189430+tolu0x@users.noreply.github.com>
2024-01-08 20:23:53 +0100

    MazeSolver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,44 @@
+const dirs = [
+  [0, 1],   // top
+  [1, 0],   // right
+  [0, -1],  //bottom
+  [-1, 0],   //left
+];
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+
+  // 1. Base Case
+  // off the map
+  if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) {
+    return false;
+  }
+
+  // on a wall
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
+
+  if (curr.x === end.x && curr.y === end.y) {
+    return true;
+  }
+
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
+
+  // 3. recurse
+  // pre
+  path.push(curr);
+
+  //recurse
+  for (let i = 0; i < path.length; ++i) {
+
+  }
+
+  // post
+  path.pop();
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
